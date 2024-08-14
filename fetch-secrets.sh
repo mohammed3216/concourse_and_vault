e3f612f49ac2e5fbad4a7117c9cba8ae475c7843
mohammed3216 <mdop269@gmail.com>
2024-08-14 12:18:01 +0530

    Update fetch-secrets.sh

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-# Set the Vault address and token (you may need to adjust these)
-export VAULT_ADDR='http://192.168.3.59:8200'
+# Set the Vault address and token
+export VAULT_ADDR='http://vault:8200'
 export VAULT_TOKEN='test'
 
 # Retrieve the secrets from Vault
-export SECRET_USERNAME=$(vault kv get -field=email secret/myapp)
-export SECRET_PASSWORD=$(vault kv get -field=password secret/myapp)
+email=$(vault kv get -field=email secret/myapp)
+password=$(vault kv get -field=password secret/myapp)
 
-# Echo the retrieved secrets to the console (optional, for debugging)
-echo "Email: $SECRET_USERNAME"
+# Export the secrets as environment variables
+export SECRET_EMAIL="$email"
+export SECRET_PASSWORD="$password"
+
+# Debugging: Print the retrieved secrets
+echo "Email: $SECRET_EMAIL"
 echo "Password: $SECRET_PASSWORD"
